e7b572bf16b21a2d1ece74f7da363da35cc27afd
Hridoy Nandi <133546308+Hrk01221@users.noreply.github.com>
2025-03-19 02:34:05 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://easyjudge.onrender.com']
 
 app.use(express.json());
 app.use(cookieParser());
