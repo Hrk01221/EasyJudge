0b41d5881687ca7802f829ba271fee2fa5517b89
Hrk01221 <hrkisdead01221@gmail.com>
2026-09-18 16:43:35 +0600

    tweak

--- JudgeBackend/app/core/config.py
+++ JudgeBackend/app/core/config.py
@@ -4,10 +4,6 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
     API_V1_STR : str = "/api/v1"
-    GITHUB_TOKEN : str
-    GITHUB_OWNER : str
-    GITHUB_REPO : str 
-    GITHUB_BRANCH : str
     FRONTEND_API : str
 
 settings = Settings()
